3fc3dc6e121af7cedf7cc6a0f834ec4de47dc0aa
Don Park <donp@donp.org>
2023-07-13 14:06:57 -0700

    moar sql

--- src/sql.rs
+++ src/sql.rs
@@ -6,4 +6,10 @@ pub(crate) fn init() {
 
     let mut client = Client::connect(&config.psql, NoTls).unwrap();
     log::info!("sql connected");
+    embedded::migrations::runner().run(&mut client).unwrap();
+}
+
+mod embedded {
+    use refinery::embed_migrations;
+    embed_migrations!("./sql");
 }
