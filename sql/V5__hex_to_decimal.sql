12f9f25f0a1c5015e627fe2774b227895a5181ac
Don Park <donp@donp.org>
2023-11-04 17:21:57 +0700

    hex_to_decimal.sql

---
+++ sql/V5__hex_to_decimal.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION hex_to_decimal3(hex_string text)
+ RETURNS numeric
+ LANGUAGE plpgsql
+ IMMUTABLE
+AS $function$
+declare
+    hex_string_lower text := lower(hex_string);
+    i int;
+    digit int;
+    s numeric := 0;
+begin
+    for i in 1 .. length(hex_string) loop
+        digit := position(substr(hex_string_lower, i, 1) in '0123456789abcdef') - 1;
+        if digit < 0 then
+            raise '"%" is not a valid hexadecimal digit', substr(hex_string_lower, i, 1) using errcode = '22P02'; 
+        end if;
+        s := s * 16 + digit;
+    end loop;
+   
+    return s;
+end
+$function$;
+
